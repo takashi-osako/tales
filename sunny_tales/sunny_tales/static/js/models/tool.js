b9dee5cf6c3236ada24fbfb8b3eded1f9d05cd68
Takashi Osako <tosako@wgen.net>
2013-06-02 22:07:16 -0400

    fix remove x button

--- sunny_tales/sunny_tales/static/js/models/tool.js
+++ sunny_tales/sunny_tales/static/js/models/tool.js
@@ -17,9 +17,9 @@ Tool = Backbone.Model.extend({
 		j_htmlObject.text(this.get("value"));
 
 		// Add relevant classes
-		var wrapper = $("<div class='resizable draggable' style='overflow:hidden;'></div>");
+		var wrapper = $("<div class='resizable draggable' style='overflow:hidden; height:inherit; width: inherit'></div>");
 		// Wrap around the html from config and convert to html string
-		var close_x = $("<div style='overflow: visible;'><img id=close_x class='close_x' src='/static/png/glyphicons_197_remove.png' style='position: absolute;display:none;top:-20px;cursor: pointer;'></div>")
+		var close_x = $("<div style='overflow: visible;'><div id=close_x class='close_x' style='background: url(/static/png/glyphicons_197_remove.png) no-repeat scroll center left transparent; position: absolute;display:none;top:-20px;left:-20px;cursor: pointer;height:28px;width:28px;'></div></div>")
 
 		j_htmlObject.appendTo(wrapper);
 		wrapper.appendTo(close_x)
